b1ee11094f05a757ad9e02a8771e3c009fa19930
DetGrey <anenovlar@gmail.com>
2024-02-24 16:41:46 +0100

    Selection sort finished

--- app/src/main/java/com/example/sorting/insertion-sort.kt
+++ app/src/main/java/com/example/sorting/insertion-sort.kt
@@ -1,2 +1,56 @@
 package com.example.sorting
 
+fun main() {
+    val unsortedStrings = listOf("banana", "apple", "orange", "grape", "kiwi",
+        "pineapple", "watermelon", "strawberry", "blueberry", "raspberry",
+        "peach", "pear", "mango", "lemon", "lime",
+        "cherry", "plum", "apricot", "blackberry", "papaya"
+    )
+
+    insertionSort(unsortedStrings)
+
+//    println("Unsorted:")
+//    println(unsortedStrings)
+//    println("Sorted:")
+//    println()
+}
+
+/* Implement the [insertion sort](https://www.youtube.com/watch?v=O0VbBkUvriI) algorithm with
+the following elements:
+
+val unsortedStrings = listOf("banana", "apple", "orange", "grape", "kiwi",
+ "pineapple", "watermelon", "strawberry", "blueberry", "raspberry",
+ "peach", "pear", "mango", "lemon", "lime",
+ "cherry", "plum", "apricot", "blackberry", "papaya"
+)
+
+- Understand the algorithm with pen and paper
+- [Visualisation](https://youtu.be/JU767SDMDvA?si=3pT2PlxJW48F3wll)
+ */
+
+fun insertionSort(list: List<String>) {
+    val sortedList: MutableList<String> = list.toMutableList()
+    // Mark first word as sorted
+    // Look at next word and see if it should come before the first
+    // if the two words should be swapped, do that, else mark as sorted
+
+    var i = 1
+
+    while (i < list.size) {
+
+        for (j in 0..i) {
+            if (sortedList[i] < sortedList[j]) {
+                val temp = sortedList[i]
+                sortedList[j+1] = sortedList[j]
+                sortedList[j] = temp
+            }
+        }
+
+        println(list[i])
+        println(sortedList)
+        i++
+
+    }
+
+
+}
--- app/src/main/java/com/example/sorting/selection-sort.kt
+++ app/src/main/java/com/example/sorting/selection-sort.kt
@@ -1,2 +1,71 @@
 package com.example.sorting
 
+fun main() {
+    val unsortedNumbers = listOf(29, 15, 8, 47, 63, 22, 56, 31, 74, 19, 33, 41, 52, 60, 10, 88, 92, 38, 45, 17)
+
+    println("Unsorted:")
+    println(unsortedNumbers)
+    println("Sorted:")
+    println(selectionSort2(unsortedNumbers))
+}
+
+/* Implement the [selection sort](https://www.youtube.com/watch?v=3hH8kTHFw2A) algorithm
+with the following elements:
+
+val unsortedNumbers = listOf(29, 15, 8, 47, 63, 22, 56, 31, 74, 19, 33, 41, 52, 60, 10, 88, 92, 38, 45, 17)
+
+- Understand the algorithm with pen and paper
+- [Visualisation](https://www.youtube.com/watch?v=Iccmrk2ZWoc)
+
+ */
+
+fun selectionSort(list: List<Int>): MutableList<Int> {
+    val unsortedList: MutableList<Int> = list.toMutableList()
+    val sortedList: MutableList<Int> = mutableListOf()
+
+    var i = 0
+
+    while (i < list.size) {
+
+        var min = unsortedList[0]
+
+        for (num in unsortedList) {
+            if (num < min) {
+                min = num
+            }
+        }
+
+        sortedList.add(min)
+        unsortedList.remove(min)
+
+        i++
+    }
+
+    return sortedList
+}
+
+fun selectionSort2(list: List<Int>): MutableList<Int> {
+    val sortedList: MutableList<Int> = list.toMutableList()
+
+    var sortedIndex = 0
+
+    while (sortedIndex < list.size) {
+
+        var min = sortedIndex
+
+        for (i in sortedIndex until sortedList.size) {
+            if (sortedList[i] < sortedList[min]) {
+                min = i
+            }
+        }
+
+        // SWAP THE LOWEST NUM WITH THE FIRST UNSORTED NUM
+        val temp = sortedList[min]
+        sortedList[min] = sortedList[sortedIndex]
+        sortedList[sortedIndex] = temp
+
+        sortedIndex++
+    }
+
+    return sortedList
+}
